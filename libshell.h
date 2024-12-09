6d695d75eacc9d32712448ccd2448c52ed9457e4
RubenSay <rubensayansfortes@gmail.com>
2024-12-09 10:52:02 +0100

    Update libshell.h
    nada

--- libshell.h
+++ libshell.h
@@ -43,5 +43,5 @@ char * Ejecutable (char *s, SEARCH dirs);
 int Execpve(char *tr[], char **NewEnv, int * pprio, SEARCH dirs);
 void exec_chop(char *args[], int counter, char *new_env[],char *cmd_args[],int control);
 void Cmd_listjobs(PRO procesos);
-void gettuid();
+
 #endif
